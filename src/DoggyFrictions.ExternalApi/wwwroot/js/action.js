4402048cdf22c35051c70d5e39656a7f3c67143f
Sergei Sokratov <sergeysokratov@gmail.com>
2025-11-16 14:14:03 -0800

    feat(UX): further simplify the UI

--- src/DoggyFrictions.ExternalApi/wwwroot/js/action.js
+++ src/DoggyFrictions.ExternalApi/wwwroot/js/action.js
@@ -1,19 +1,15 @@
-﻿function PayerModel(payerData) {
-    var _this = this;
-    this.Id = payerData.Id || 0;
-    this.ParticipantId = ko.observable(payerData.ParticipantId);
-    this.Amount = ko.observable(payerData.Amount);
-    this.HasFocus = ko.observable(false);
-}
-
-function ActionModel(actionData, sessionModel, isEdit) {
+﻿function ActionModel(actionData, sessionModel, isEdit) {
     var _this = this;
     _this.Id = actionData.Id || 0;
     _this.Session = sessionModel;
     _this.Date = ko.observable((actionData.Date || Date()).formatDate());
-    _this.Payers = ko.observableArray(_.map(actionData.Payers || [], function (payerData) {
-        return new PayerModel(payerData);
-    }));
+    
+    // Always ensure we have exactly one payer
+    var payersData = actionData.Payers || [];
+    if (payersData.length === 0) {
+        payersData = [{ ParticipantId: sessionModel.Participants[0].Id }];
+    }
+    _this.PayerId = ko.observable(payersData.ParticipantId);
 
     // Always ensure we have exactly one consumption
     var consumptionsData = actionData.Consumptions || [];
@@ -39,32 +35,9 @@ function ActionModel(actionData, sessionModel, isEdit) {
             return current + Number(next.Amount());
         }, 0);
     });
-    _this.PayerNames = ko.computed(function () {
-        return _.reduceRight(_this.Payers(), function (current, next) {
-            var participantName = _this.Session.GetParticipant(next.ParticipantId()).Name();
-            return (current.length ? (current + ', ') : '') + participantName;
-        }, '');
+    _this.PayerName = ko.computed(function () {
+        return _this.Session.GetParticipant(_this.PayerId()).Name()];
     });
-    this.AddPayer = function () {
-        _.each(_this.Payers(), function (c) { c.HasFocus(false); });
-        var payerModel = new PayerModel({ ParticipantId: _this.Session.Participants()[0].Id });
-        _this.Payers.push(payerModel);
-        payerModel.HasFocus(true);
-
-        // Calculate unpaid rest
-        var consumedAmount = _.reduce(_this.Consumptions(), function (current, next) {
-            return current + Number(next.Amount() || 0);
-        }, 0);
-        var paidAmount = _.reduce(_this.Payers(), function (current, next) {
-            return current + Number(next.Amount() || 0);
-        }, 0);
-        payerModel.Amount(consumedAmount - paidAmount);
-
-        window.App.Functions.ReapplyJQuerryStuff();
-    }
-    this.DeletePayer = function (payerModel) {
-        _this.Payers.remove(payerModel);
-    }
 
     this.ToggleConsumer = function (participant) {
         if (!_this.IsEdit()) {
@@ -89,42 +62,31 @@ function ActionModel(actionData, sessionModel, isEdit) {
             window.App.Functions.Move('#/Session/' + _this.Session.Id)();
         });
     }
-    this.SaveAndNew = function() {
-        var operation = _this._createSaveOperation();
-        window.App.Functions.Process(operation);
-        $.when(operation).done(function (actionData) {
-            window.App.Functions.Move('#/Session/' + _this.Session.Id + '/Action/Create')();
-        });
-    }
     this._createSaveOperation = function() {
         var serialized = {
             Id: _this.Id,
             SessionId: _this.Session.Id,
             Description: _this.Description(),
             Date: _this.Date().extractDate(),
-            Payers: _.map(_this.Payers(), function (payerModel) {
-                return {
-                    Id: payerModel.Id,
-                    ParticipantId: payerModel.ParticipantId(),
-                    Amount: payerModel.Amount()
-                }
-            }),
-            Consumptions: _.map(_this.Consumptions(), function (consumptonModel) {
-                return {
-                    Id: consumptonModel.Id,
-                    Amount: consumptonModel.Amount(),
-                    SplittedEqually: consumptonModel.IsAuto(),
-                    Consumers: _.map(_.filter(consumptonModel.Consumers(), function (consumerModel) {
-                        return consumerModel.IsActive();
-                    }), function (consumerModel) {
-                        return {
-                            Id: consumerModel.Id,
-                            ParticipantId: consumerModel.ParticipantId,
-                            Amount: consumerModel.Amount()
-                        };
-                    })
-                }
-            })
+            Payers: [{                
+                Id: 0,
+                ParticipantId: _this.PayerId(),
+                Amount: _this.Consumptions()[0].Amount()
+            }],
+            Consumptions: {
+                Id: _this.Consumptions()[0].Id,
+                Amount: _this.Consumptions()[0].Amount(),
+                SplittedEqually: true,
+                Consumers: _.map(_.filter(_this.Consumptions()[0].Consumers(), function (consumerModel) {
+                    return consumerModel.IsActive();
+                }), function (consumerModel) {
+                    return {
+                        Id: consumerModel.Id,
+                        ParticipantId: consumerModel.ParticipantId,
+                        Amount: consumerModel.Amount()
+                    };
+                })
+            }
         };
         var operation = (_this.Id == 0
             ? $.post('Api/Actions/' + _this.Session.Id, serialized)
